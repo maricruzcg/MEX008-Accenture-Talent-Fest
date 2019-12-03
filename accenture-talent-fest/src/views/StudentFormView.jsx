846f83076cf7f4c163cbf39a5c60cbb28a00fe9f
SolGee <solsv00@hotmail.com>
2019-12-02 21:53:57 -0600

    Add sidebar

--- accenture-talent-fest/src/views/StudentFormView.jsx
+++ accenture-talent-fest/src/views/StudentFormView.jsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import StudentForm from '../components/StudentForm';
+import Navbar from '../components/Navbar';
 
 class StudentFormView extends Component {
   render() {
     return (
       <Fragment>
+        <Navbar />
         <StudentForm />
       </Fragment>
     );
