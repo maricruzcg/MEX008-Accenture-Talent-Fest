3f3234be0ca6721a5323217bff700b8e0b7a0f31
SolGee <solsv00@hotmail.com>
2019-12-02 22:15:03 -0600

    Add basic requirement view

--- accenture-talent-fest/src/components/StudentForm.jsx
+++ accenture-talent-fest/src/components/StudentForm.jsx
@@ -1,40 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 class StudentForm extends Component {
   render() {
     return (
-      <form action="">
-        <label htmlFor="">
-          <p>Número de Cuenta</p>
-        </label>
-        <input type="text" value="3489153896" readonly/>
-        <label htmlFor="">
-          <p>Nombre</p>
-        </label>
-        <input type="text" value="Carolina Ramírez Jiménez" readonly/>
-        <label htmlFor="">
-          <p>Fecha de Nacimiento</p>
-        </label>
-        <input type="text" value="10 marzo 1999" readonly/>
-        <label htmlFor="">
-          <p>Carrera</p>
-        </label>
-        <input type="text" value="Comunicación" readonly/>
-        <label htmlFor="">
-          <p>Status</p>
-        </label>
-        <input type="text" value="Estudiante" readonly/>
-        <label htmlFor="">
-          <p>Semestre</p>
-        </label>
-        <input type="text" value="Octavo" readonly/>
-        <label htmlFor="">
-          <p>Año de Ingreso</p>
-        </label>
-        <input type="text" value="2015" readonly/>
-        <p>Recuerda revisar tus datos personales. <br/> Comunícate al 57 45 36 32</p>
+      <Fragment>
+        <h2>Verifica tu información</h2>
 
-      </form>
+        <form action=''>
+          <label htmlFor=''>
+            <p>Nombre</p>
+          </label>
+          <input type='text' value='Carolina Ramírez Jiménez' readonly />
+          <label htmlFor=''>
+            <p>Fecha de Nacimiento</p>
+          </label>
+          <input type='text' value='10 marzo 1999' readonly />
+          <label htmlFor=''>
+            <p>Carrera</p>
+          </label>
+          <input type='text' value='Comunicación' readonly />
+          <label htmlFor=''>
+            <p>Status</p>
+          </label>
+          <input type='text' value='Estudiante' readonly />
+          <label htmlFor=''>
+            <p>Semestre</p>
+          </label>
+          <input type='text' value='Octavo' readonly />
+          <label htmlFor=''>
+            <p>Año de Ingreso</p>
+          </label>
+          <input type='text' value='2015' readonly />
+          <Link to='/basic-requirements'>
+            <button>Confirmar</button>
+          </Link>
+          <p>
+            ¿Tu información no corresponde? <br /> Comunícate al 57 45 36 32
+          </p>
+        </form>
+      </Fragment>
     );
   }
 }
---
+++ accenture-talent-fest/src/views/BasicRequirementsView.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Navbar from '../components/Navbar';
+
+class BasicRequirements extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+      </Fragment>
+    );
+  }
+}
+
+export default BasicRequirements;
