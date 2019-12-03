721f3700b9fec7c33389ad500b5269adf430510f
maricruzcg <maricruz7@live.com.mx>
2019-12-02 22:33:31 -0600

    cambios en componente

---
+++ accenture-talent-fest/src/components/BasicRequirements.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BasicRequirements extends Component {
+  render() {
+    return (
+      <h1>Requisitos básicos para iniciar proceso de titulación</h1>
+    );
+  }
+}
+
+export default BasicRequirements;
